09a37cdcc79fd5abfe68bcfab047ee323091445c
natali-gracia <natali1gracia@gmail.com>
2020-09-21 14:38:53 +0300

    modified input with useref hook

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,17 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 export const TodoForm: React.FC = () => {
 
-    const [title, setTitle] = useState<string>('')
+    // const [title, setTitle] = useState<string>('')
+    const ref = useRef<HTMLInputElement>(null)
 
-    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.target.value)
-    }
+    // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setTitle(event.target.value)
+    // }
 
     const keyPressHandler = (event: React.KeyboardEvent) => {
         if (event.key === 'Enter') {
-            console.log(title)
-            setTitle('')
+            console.log(ref.current!.value)
+            ref.current!.value = ''
+            // console.log(title)
+            // setTitle('')
             
         }
     }
@@ -19,8 +22,9 @@ export const TodoForm: React.FC = () => {
     return (
         <div className="input-field mt2">
             <input 
-                onChange={changeHandler}
-                value={title}
+                // onChange={changeHandler}
+                // value={title}
+                ref={ref}
                 type="text" 
                 id='title' 
                 placeholder='Add a task'
