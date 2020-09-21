ee7fba7e727be886ea765744ef362174e7a9cb1b
natali-gracia <natali1gracia@gmail.com>
2020-09-21 17:02:56 +0300

    used localstorage for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Navbar } from './components/Navbar'
 import { TodoForm } from './components/TodoForm'
 import { TodoList } from './components/TodoList'
@@ -10,13 +10,21 @@ const App: React.FC = () => {
 
   const [todos, setTodos] = useState<ITodo[]>([])
 
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
+    setTodos(saved)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos)) 
+  }, [todos]) 
+
   const addHandler = (title: string) => {
     const newTodo: ITodo = {
       title: title,
       id: Date.now(),
       completed: false
     }
-    // setTodos([newTodo, ...todos])
     setTodos(prev => [newTodo, ...prev])
 
     
